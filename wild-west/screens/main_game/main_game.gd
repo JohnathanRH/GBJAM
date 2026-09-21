1578db2e0eada9c27d17e826a4797d92756c66e1
Muhammad Guntur Ricky Adhitya <gunturpdng@gmail.com>
2026-09-21 16:56:41 +0800

    add shake effect when player get damaged and attack enemy

--- wild-west/screens/main_game/main_game.gd
+++ wild-west/screens/main_game/main_game.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 var enemy_scn = preload("res://entities/enemy/enemy.tscn")
 
+@export var random_strength: float = 2.0
+@export var shake_duration: float = 0.2
+var tween: Tween
+
 func _enter_tree() -> void:
 	$Player.entity_resource = SaveManager.file.player_resource
 	var stage_num: int = SaveManager.file.stage_number
+	
+	$Player.damage_received.connect(_on_player_damaged)
+	$Player.auto_fired.connect(_on_player_fired)
 	
 	var stage: Stage = Stages.fetch(stage_num)
 	for i in stage.enemies.size():
@@ -31,3 +38,25 @@ func _on_victory_delay_timeout() -> void:
 			SaveManager.save()
 			
 			get_tree().change_scene_to_file("res://screens/bullet_pick/bullet_pick.tscn")
+
+func _on_player_damaged() -> void:
+	_shake_camera()
+
+func _on_player_fired() -> void:
+	_shake_camera()
+
+func _shake_camera() -> void:
+	if tween and tween.is_running():
+		tween.kill()
+	
+	tween = create_tween().set_parallel(false)
+	
+	var original_offset = %Camera.offset
+	
+	for i in range(5):
+		var offset = Vector2(
+			original_offset.x + randf_range(-random_strength, random_strength),
+			original_offset.y + randf_range(-random_strength, random_strength)
+			)
+		tween.tween_property(%Camera, "offset", offset, shake_duration / 5.0)
+	tween.tween_property(%Camera, "offset", original_offset, shake_duration / 5.0)
