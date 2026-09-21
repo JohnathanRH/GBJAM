b5e5391544648fcf6273ebb496b80ec3403cdb6f
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2026-09-21 18:05:41 +0800

    Crash fix

--- wild-west/screens/bullet_pick/bullet_pick.gd
+++ wild-west/screens/bullet_pick/bullet_pick.gd
@@ -42,14 +42,15 @@ func save_game() -> void:
 	SaveManager.save()
 
 func set_loot_pointer(value: int) -> void:
-	var old_button: SelectableBullet = $LootContainer.get_child(loot_pointer)
-	old_button.unselected()
-	if value >= $LootContainer.get_child_count():
-		loot_pointer = 0
-	else:
-		loot_pointer = value
-	var button: SelectableBullet = $LootContainer.get_child(loot_pointer)
-	button.selected()
+	if $LootContainer.get_child_count() > 0:
+		var old_button: SelectableBullet = $LootContainer.get_child(loot_pointer)
+		old_button.unselected()
+		if value >= $LootContainer.get_child_count():
+			loot_pointer = 0
+		else:
+			loot_pointer = value
+		var button: SelectableBullet = $LootContainer.get_child(loot_pointer)
+		button.selected()
 
 func _on_bullet_selected() -> void:
 	var selected_btn: SelectableBullet = $SelectablesContainer.selected_ui
